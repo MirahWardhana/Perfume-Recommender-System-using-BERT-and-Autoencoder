e898410ae82f767345671e44f44b44e06d2afe33
Mirah Wardhana <mirah.indira@gmail.com>
2025-02-20 11:02:05 +0800

    add data and interface

--- program/myapp/urls.py
+++ program/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("feature1/", views.feature1, name="feature1"),  
+    path("feature2/", views.feature2, name="feature2"),  
 ]
--- program/myapp/views.py
+++ program/myapp/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+import os
+import pandas as pd
 
-# Create your views here.
+# Ambil path file dataset secara dinamis
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, 'dataset/preprocessed_data.xlsx')
+
+# Fungsi untuk membaca dataset dengan aman
+def load_dataset():
+    try:
+        df = pd.read_excel(DATA_PATH)
+        # Ganti spasi dengan underscore (_) dalam nama kolom
+        df.columns = df.columns.str.replace(" ", "_")
+        return df
+    except Exception as e:
+        print(f"Error loading dataset: {e}")
+        return pd.DataFrame()  
+
+
+def convert_df_to_list(df):
+    """Konversi DataFrame ke list of dictionaries."""
+    return df.to_dict(orient='records')
+
+# Views
 def index(request):
     return render(request, "myapp/index.html")
+
+def feature1(request):
+    return render(request, 'myapp/feature1.html')
+
+def feature2(request):
+    if request.method == 'GET':
+        df = load_dataset()  # Muat ulang dataset
+        df_list = convert_df_to_list(df)
+        return render(request, "myapp/feature2.html", {
+            "datas": df_list[:100]  # Kirim 100 data pertama ke template
+        })
